605509729c873f928d9071ab74274ce671d59799
edward12699 <jinxiaoxiang@jinxiaoxiangdeMacBook-Pro.local>
2015-11-18 00:52:14 +0800

    差不多了吧
    如果不禁止默认时间，要么就添加点击事件的例外，

--- script/ani.js
+++ script/ani.js
@@ -1,10 +1,4 @@
-/**
- * Created by xiaoxiang on 2015/10/14.
- * 页面上拉到底以后的过程，此时article的个数已经知道
- * 页面采取清空article的方式，所以初加载的时候直接把内容先放到本地存储，再从本地存储放到页面上
- * 理想方式是后台修改HTML CSS JS文件，不知道分成几份和整成一份性能和修改难易度。
- * 页面只放3个article，因为有页面背景啊，不然就可以用一个article。
- */
+
 var content=new Array();//存放H5代码，加载完成后复制给本地存储，这里直接把内容先写死。
 var count=0;//需要页面的index（index从0开始，计数从1开始）
 var start;
@@ -12,10 +6,8 @@ var touchL;
 var mn=-1;
 var n=2;//article计数
 var scw=screen.height;
-content=[
-		"<div class='crying b1'><img src='../image/crying.png'></div><div class='crying b2'><img src='../image/crying.png'></div><div class='crying b3'><img src='../image/crying.png'></div><div class='crying b4'><img src='../image/crying.png'></div><div  class='mg' ><div class='man man1'><img src='../image/man.png'></div><div class='girl girl1'><img src='../image/girl.png'></div></div><span class='arrow'></span>"
-	,'<div class="crying1"><img src="../image/crying.png" class="c1"><img src="../image/arrow_yellow.png" class="c2"><img src="../image/girl.png" class="c3"><img src="../image/girl.png" class="c4"><img src="../image/girl.png"class="c5"><img src="../image/girl.png"class="c6"><img src="../image/girl.png"class="c7"><img src="../image/girl.png"class="c8"><img src="../image/girl.png"class="c9"></div>'
-]
+var count=0;
+var body;
 localStorage.content=content;//从AJAX读取存放以后本来应该再转成数组，这里省略这一步。
 function getClientsize() {
 		if (document.compatMode == "BackCompat")
@@ -31,13 +23,13 @@ function getClientsize() {
 function getStart(ev){
 		 if(ev.targetTouches.length==1){
 				 start= ev.targetTouches[0].clientY;
-				 ev.preventDefault();
+				// ev.preventDefault();
 		 }
 }
 function getTouch(ev){
 		if(ev.targetTouches.length==1){
 				touchL=ev.targetTouches[0].clientY-start;
-				ev.preventDefault();
+				//ev.preventDefault();
 		}
 
 }
@@ -46,54 +38,18 @@ function getEnd(ev,mn) {
 		var that=this;
 		//pull up
 				if (touchL <- 10) {
-						count = count + 1;
-						$(that).empty();
-						body.style.transform = "translateY(-" + count * screen.height + "px)";
-//如果已经到页面底部了，记载的内容就变成第一页
-						if (count >= n) {
-								count = 0;
-						}
-						$("article:eq(2)").append(content[count]);//猜测：这里是在transform执行的时候进行还是完成transform事件以后进行。所以能看到动画
-				//增加第三个article，删除第一个article，设置动画，掩盖变化的痕迹
-						$("body").css({
-								"transition":"transform 0s linear",
-								"transform":"translateY(-"+scw+"px)"
-						})
-						$("article:eq(0)").remove();
-						$("body").append("<article></article>");
-						$("article:eq(2)").bind("touchend",getEnd);
-						//还原
-						$("article").css("width",screen.width+"px");
-						$("article").css("height",screen.height+"px");
-						$("body").css({
-								"transition":"transform 0.5s linear"
-						})
+                    count = count + 1;
+                    if(count<=1) {
+                        count=1;
+                        body.style.transform = "translateY(-" + count * screen.height + "px)";
+                    }
 				}
 				//pull down
 				 else if (touchL>10) {
 						count = count - 1;
-						$(that).empty();
-						body.style.transform = "translateY(" + count * screen.height + "px)";
-//如果已经到页面顶部了，记载的内容就变成最后一页
-						if (count < 0) {
-								count = n-1;
-						}
-						$("article:eq(0)").append(content[count]);//猜测：这里是在transform执行的时候进行还是完成transform事件以后进行。所以能看到动画
-						//增加第1个article，删除第3个article，设置动画，掩盖变化的痕迹
-						$("body").css({
-								"transition":"transform 0s linear",
-								"transform":"translateY(-"+scw+"px)"
-						})
-						$("article:eq(2)").remove();
-						$("body").prepend("<article></article>");
-						$("article:eq(0)").bind("touchend",getEnd);
-						$("article").css("width",screen.width+"px");
-						$("article").css("height",screen.height+"px");
-						$("body").css({
-								"transition":"transform 0.5s linear"
-						})
-				}
-		ev.stopPropagation();
+                    if(count>=0){
+					count=0;	body.style.transform = "translateY(" + count * screen.height + "px)";
+				}}
 }
 window.onload=function()
 {
@@ -104,18 +60,13 @@ window.onload=function()
 		$("article").css("width",screen.width+"px");
 		$("article").css("height",screen.height+"px");
 		body=document.body;
-		$("body").css("transform","translateY(-"+scw+"px)")
 		body.addEventListener("touchstart",getStart,false);
 		body.addEventListener("touchmove",getTouch,false);
 		$("article").bind("touchend",getEnd);
-		$(".clickme").click(function(){
-var article=document.createElement("article");
-				article.appendChild(document.createTextNode("sddfwe\n\n\n"));
-				var first=document.body.firstChild;//得到页面的第一个元素
-				document.body.insertBefore(article,first);
-				body.style.transform="translateY(-54px)";
-
-		})
+    $(".scaleout").click(function(){
+        alert("s")
+        $(".img img:nth-child(1)").fadeIn();
+    })
 }
 
 
